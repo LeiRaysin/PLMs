d09ea26b80912645d2182e336f7502cc774c56db
Rezun <rezunlsj@outlook.com>
2024-07-12 18:27:52 +0800

    init version

---
+++ LoRA/examples/My_NLG/train_run.bat
@@ -0,0 +1,1 @@
+python .\LoRA\examples\My_NLG\src\gpt2_finetuning.py --train_data .\LoRA\examples\My_NLG\data\e2e\train.jsonl --valid_data .\LoRA\examples\My_NLG\data\e2e\valid.jsonl --init_checkpoint .\LoRA\examples\My_NLG\pretrained_checkpoints\gpt2-pytorch_model.bin
